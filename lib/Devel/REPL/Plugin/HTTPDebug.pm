21986910415233a41abccd8935730c3f05db8327
Taiyoh Tanaka <sun.basix@gmail.com>
2009-09-22 15:10:39 +0900

    initial commit

---
+++ lib/Devel/REPL/Plugin/HTTPDebug.pm
@@ -0,0 +1,33 @@
+package Devel::REPL::Plugin::HTTPDebug;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Devel::REPL::Plugin::HTTPDebug -
+
+=head1 SYNOPSIS
+
+  use Devel::REPL::Plugin::HTTPDebug;
+
+=head1 DESCRIPTION
+
+Devel::REPL::Plugin::HTTPDebug is
+
+=head1 AUTHOR
+
+taiyoh E<lt>sun.basix@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
